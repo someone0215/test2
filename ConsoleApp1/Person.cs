76c67002a5d5e75c242582425cdbced6a25d00e7
Administrator <Administrator@PC-20211102LKDW>
2022-04-26 10:49:23 +0800

    添加项目文件。

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   public abstract class Notebook
+    {
+        public abstract void Getnb();
+
+    }
+    public class Dell:Notebook
+    {
+        public override void Getnb()
+        {
+            Console.WriteLine("我是戴尔电脑");
+        }
+    }
+    public class Lenove : Notebook
+    {
+        public override void Getnb()
+        {
+            Console.WriteLine("我是联想电脑");
+        }
+    }
+    public class Help : Notebook
+    {
+        public override void Getnb()
+        {
+            Console.WriteLine("我是惠普电脑");
+        }
+    }
+}
